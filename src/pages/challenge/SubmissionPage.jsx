411d7f868a6a2a25149436246fc43f71688872e7
Yassine Mili <ya.mili@icloud.com>
2025-04-02 23:26:34 +0100

    init the submission page

---
+++ src/pages/challenge/SubmissionPage.jsx
@@ -0,0 +1,39 @@
+import bg from "../../assets/gif/scoreboard.webp";
+import { useEffect, useState } from "react";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+import SubmissionCard from "./component/SubmissionCard";
+
+function SubmisionPage() {
+  const [showFrame, setShowFrame] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowFrame(true);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <div className="flex flex-col items-center justify-center w-full mt-[60px]">
+      <img
+        src={bg}
+        alt=""
+        className="z-0 fixed w-full h-full bg-center top-0 left-0 bg-contain"
+      />
+      {showFrame && (
+        <motion.div
+          className="w-full flex flex-col gap-[20px]"
+          initial={{ opacity: 0, y: 50 }} // Start position
+          animate={{ opacity: 1, y: 0 }} // End position
+          transition={{ duration: 0.8, ease: "easeOut" }} // Smooth animation
+        >
+          {/* we will use the map function here after fetcing the data */}
+          <SubmissionCard />
+        </motion.div>
+      )}
+    </div>
+  );
+}
+
+export default SubmisionPage;
