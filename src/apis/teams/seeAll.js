18224a7b88e57556b7302325d966c0d12b7ccb20
Yassine Mili <ya.mili@icloud.com>
2025-04-04 23:50:52 +0100

    not that big deal

--- src/apis/teams/seeAll.js
+++ src/apis/teams/seeAll.js
@@ -1,19 +1,19 @@
 import API from "../axiosInstance";
 import { useQuery } from "@tanstack/react-query";
 const fetchTeams = async () => {
-    try {
-      const response = await API.get("/teams");
-      console.log("Response Data:", response.data);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching teams:", error);
-      throw error; // حتى يتمكن React Query من التعامل مع الخطأ
-    }
-  };
+  try {
+    const response = await API.get("/teams");
+    console.log("Response Data:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching teams:", error);
+    throw error; // حتى يتمكن React Query من التعامل مع الخطأ
+  }
+};
 
-export const useTeams = ()=>{
-   return useQuery({
-    queryKey:["Teams"],
+export const useTeams = () => {
+  return useQuery({
+    queryKey: ["Teams"],
     queryFn: fetchTeams,
-   })
+  })
 }
