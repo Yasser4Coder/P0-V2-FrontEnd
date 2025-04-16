c02b2706f7a9cc6b9025dd415f9ea718e0354829
Yassine Mili <ya.mili@icloud.com>
2025-04-17 00:06:01 +0100

    optmising some logic

--- src/pages/gates/Gates.jsx
+++ src/pages/gates/Gates.jsx
@@ -6,8 +6,41 @@ import { motion } from "framer-motion";
 import WorningTitle from "../../components/WorningTitle";
 import GateCard from "./components/GateCard";
 
+const initialGates = [
+  {
+    open: false,
+    date: "2025-04-16T11:24:40.000Z",
+    number: 1,
+    title: "Igris: The Red Knight",
+    desc: "The system is activated. Discover the challenges waiting for you in the gates. You will play against time and hard challenges.",
+  },
+  {
+    open: false,
+    date: "2025-04-16T11:24:40.000Z",
+    number: 2,
+    title: "Igris: The Red Knight",
+    desc: "The system is activated. Discover the challenges waiting for you in the gates. You will play against time and hard challenges.",
+  },
+  {
+    open: false,
+    date: "2025-04-24T16:00:00.000Z",
+    number: 3,
+    title: "Igris: The Red Knight",
+    desc: "The system is activated. Discover the challenges waiting for you in the gates. You will play against time and hard challenges.",
+  },
+  {
+    open: false,
+    date: "2025-04-24T18:00:00.000Z",
+    number: 4,
+    title: "Igris: The Red Knight",
+    desc: "The system is activated. Discover the challenges waiting for you in the gates. You will play against time and hard challenges.",
+  },
+];
+
 const Gates = () => {
   const [showFrame, setShowFrame] = useState(false);
+
+  const [gates, setGates] = useState(initialGates);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -16,6 +49,18 @@ const Gates = () => {
 
     return () => clearTimeout(timer);
   }, []);
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const updatedGates = gates.map((gate) => {
+      const gateDate = new Date(gate.date);
+      return {
+        ...gate,
+        open: currentDate >= gateDate,
+      };
+    });
+    setGates(updatedGates);
+  }, [gates]);
 
   return (
     <div className="flex flex-col items-center justify-center w-full mt-[60px]">
@@ -27,41 +72,26 @@ const Gates = () => {
       {showFrame && (
         <motion.div
           className="w-full"
-          initial={{ opacity: 0, y: 50 }} // Start position
-          animate={{ opacity: 1, y: 0 }} // End position
-          transition={{ duration: 0.8, ease: "easeOut" }} // Smooth animation
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
         >
           <Frame
             extraEdit={`flex flex-col duration-2000 ease-out transition-all py-[80px] gap-[50px] px-[15px] items-center justify-center`}
           >
             <WorningTitle title={"Enter a Gate"} />
-            <div className="flex w-[80%] gap-[30px]">
-              <GateCard
-                open={true}
-                title={"igris: the red knight"}
-                desc="The system is activated. Discover the challenges waiting for you in the gates. You will play against time and hard challenges. But before that, you have to choose your character in the team."
-                gateNumber={1}
-              />
-              <GateCard
-                open={false}
-                title={"igris: the red knight"}
-                gateNumber={2}
-                date="2025-04-24T14:00:00.000Z"
-              />
-            </div>
-            <div className="flex w-[80%] gap-[30px]">
-              <GateCard
-                open={false}
-                title={"igris: the red knight"}
-                gateNumber={3}
-                date="2025-04-24T16:00:00.000Z"
-              />
-              <GateCard
-                open={false}
-                title={"igris: the red knight"}
-                gateNumber={4}
-                date="2025-04-24T18:00:00.000Z"
-              />
+            
+            <div className="grid grid-cols-2 grid-rows-2 gap-[30px] w-[80%]">
+              {gates.slice(0, 4).map((gate, index) => (
+                <GateCard
+                  key={index}
+                  open={gate.open}
+                  date={gate.date}
+                  title={gate.title}
+                  desc={gate.desc}
+                  gateNumber={gate.number}
+                />
+              ))}
             </div>
           </Frame>
         </motion.div>
