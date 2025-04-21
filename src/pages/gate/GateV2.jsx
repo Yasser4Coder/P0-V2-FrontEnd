53c4b75101d6770c0797fcb953cf4d926e0f9729
Yasser Benseghier <93477835+Yasser4Coder@users.noreply.github.com>
2025-04-21 17:10:23 +0100

    dev

--- src/pages/gate/GateV2.jsx
+++ src/pages/gate/GateV2.jsx
@@ -11,7 +11,7 @@ import igritGif from "../../assets/gif/igrit.gif";
 
 // kargalgan
 
-import kargalgan from "../../assets/gif/Kargalgan.png";
+import kargalgan from "./../../assets/gif/kargalgan.png";
 import kargalganGif from "../../assets/gif/kargalganGif.gif";
 
 // Baran
